0e19b02d4b3e2ff998aef0bf5d516a7c21af84c0
Jorge Escobar <jorge@fromzero.io>
2018-02-18 14:35:08 -0500

    step-7

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
-def my_function(first_name, last_name):
-    full_name = f'Hello {first_name} {last_name}'
-    return full_name
+def my_function(first_name, last_name, address=None, phone=None):
+    customer_data = f'{first_name} {last_name}'
+    if address is not None:
+        customer_data += f', a:{address}'
+    if phone is not None:
+        customer_data += f', p:{phone}'
+    return customer_data
 
-composed_name = my_function('John', 'Jones')
-print(composed_name)
+print(my_function('John', 'Jones'))
+print('---')
+print(my_function('Jane', 'Jones', '12 Main Street'))
+print('---')
+print(my_function('Julia', 'Jones', '80 Elm Street', '455-322-2222'))
+print('---')
+print(my_function('Robert', 'Jones', phone='881-555-2455', address='65 Oak Street'))

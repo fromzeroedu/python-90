c94e49243a1ab810c6e6d4a8616131a857a7386f
Jorge Escobar <jorge@fromzero.io>
2018-02-18 14:28:52 -0500

    step-5

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-def my_function(name):
-    print('Hello ' + name)
+def my_function(first_name, last_name):
+    print(f'Hello {first_name} {last_name}')
     print('---')
 
-my_function('John')
+my_function('John', 'Jones')

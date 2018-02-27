808098021236e4c2fffa90f651af313375cd0368
Jorge Escobar <jorge@fromzero.io>
2018-02-27 07:22:01 -0500

    step-16

--- animal.py
+++ animal.py
@@ -29,11 +29,3 @@ class Dog(Animal):
     @staticmethod
     def get_breeds():
         return ['Shitzu', 'Maltese', 'Poodle']
-
-print(Animal.get_planet())
-print(Dog.get_breeds())
-fido = Dog()
-print(fido.make_sound())
-print(fido.is_thirsty())
-fido.update_coordinates(4, 6)
-print(fido.coordinates)

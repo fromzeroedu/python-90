2e27a664cbc2fc74a46f98e5d7fc5b38bf1a9ca4
Jorge Escobar <jorge@fromzero.io>
2018-03-05 07:56:08 -0500

    step-22

--- animal_app/run.py
+++ animal_app/run.py
@@ -1,6 +1,10 @@
-import sys
+import os, sys
+
+print('__file__:', __file__)
+script_folder_name = os.path.dirname(__file__)
+print('script folder name:', script_folder_name)
+
 sys.path.append('/Users/jorescobar/python_tests')
-print(sys.path)
 
 from animal_app.canine.dog import Dog
 

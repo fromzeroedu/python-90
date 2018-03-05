7c2243c1f8416787b32c34ad3aeef0b9f312b6e1
Jorge Escobar <jorge@fromzero.io>
2018-03-05 08:30:31 -0500

    step-23

--- animal_app/run.py
+++ animal_app/run.py
@@ -4,6 +4,12 @@ print('__file__:', __file__)
 script_folder_name = os.path.dirname(__file__)
 print('script folder name:', script_folder_name)
 
+parent_relative_path = os.path.join(script_folder_name, '..')
+print('parent relative path', parent_relative_path)
+
+root_path = os.path.abspath(parent_relative_path)
+print('root path', root_path)
+
 sys.path.append('/Users/jorescobar/python_tests')
 
 from animal_app.canine.dog import Dog

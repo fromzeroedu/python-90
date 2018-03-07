72c68c8d82a5a4b4102a10dc95a4ec9f4652bcfb
Jorge Escobar <jorge@fromzero.io>
2018-03-07 07:20:55 -0500

    step-24

--- animal_app/run.py
+++ animal_app/run.py
@@ -1,11 +1,7 @@
 import os, sys
 
 sys.path.append(os.path.abspath(
-<<<<<<< HEAD
     os.path.join(os.path.dirname(__file__), '..')
-=======
-        os.path.join(os.path.dirname(__file__), '..')
->>>>>>> bf2ea48d6c5be0c1c8a0c069ee61425c5e8b0a97
     ))
 
 from animal_app.canine.dog import Dog

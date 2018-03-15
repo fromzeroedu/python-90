66a7092b6c75e2deb8c4bdc340315f38f5c12cf1
Jorge Escobar <jorge@fromzero.io>
2018-03-15 07:46:38 -0400

    step-30

--- animal_app/tests.py
+++ animal_app/tests.py
@@ -5,5 +5,8 @@ class TestPythonMethods(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(5 + 3, 8)
 
+    def test_substraction(self):
+        self.assertEqual(5 - 3, 1)
+
 if __name__ == '__main__':
     unittest.main()

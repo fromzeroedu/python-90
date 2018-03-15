ac85444b8a06afb36b8cf21483186fdcdf4c35d9
Jorge Escobar <jorge@fromzero.io>
2018-03-15 08:32:46 -0400

    step-33

--- animal_app/tests.py
+++ animal_app/tests.py
@@ -9,15 +9,16 @@ from animal_app.animal import Animal
 
 class TestAnimal(unittest.TestCase):
 
+    def setUp(self):
+        self.animal = Animal()
+
     def test_legs(self):
-        animal = Animal()
-        animal.legs = 4
-        self.assertEqual(animal.legs, 4)
+        self.animal.legs = 4
+        self.assertEqual(self.animal.legs, 4)
 
     def test_coordinates(self):
-        animal = Animal()
-        animal.update_coordinates(15, 4)
-        self.assertEqual(animal.coordinates, (15,4))
+        self.animal.update_coordinates(15, 4)
+        self.assertEqual(self.animal.coordinates, (15,4))
 
 if __name__ == '__main__':
     unittest.main()
